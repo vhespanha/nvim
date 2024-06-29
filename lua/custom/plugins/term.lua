0f7541dfb4c108721220b01cfe53a41c17524e17
vhespanha <vhespanha@tuta.io>
2024-06-29 16:23:28 -0300

    keymapping adjustments

--- lua/custom/plugins/term.lua
+++ lua/custom/plugins/term.lua
@@ -1,5 +1,14 @@
 return {
   'akinsho/toggleterm.nvim',
+  dependencies = {
+    'willothy/flatten.nvim',
+    config = true,
+    -- or pass configuration with
+    -- opts = {  }
+    -- Ensure that it runs first to minimize delay when opening file from terminal
+    lazy = false,
+    priority = 1001,
+  },
   version = '*',
   config = function()
     require('toggleterm').setup {
---
+++ lua/custom/plugins/wtf.lua
@@ -0,0 +1,41 @@
+return {
+  'vhespanha/wtf.nvim',
+  branch = 'feat/api_key_command',
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+  },
+  opts = {
+    -- Default AI popup type
+    popup_type = 'popup',
+    -- An alternative way to set your API key
+    openai_api_key_cmd = 'pass show api/openai/nvim',
+    -- ChatGPT Model
+    openai_model_id = 'gpt-4o',
+    -- Send code as well as diagnostics
+    context = true,
+    -- Set your preferred language for the response
+    language = 'english',
+    -- Any additional instructions
+    search_engine = 'phind',
+    -- Add custom colours
+    winhighlight = 'Normal:Normal,FloatBorder:CmpBorder',
+  },
+  keys = {
+    {
+      'gw',
+      mode = { 'n', 'x' },
+      function()
+        require('wtf').ai()
+      end,
+      desc = 'Debug diagnostic with AI',
+    },
+    {
+      mode = { 'n' },
+      'gW',
+      function()
+        require('wtf').search()
+      end,
+      desc = 'Search diagnostic with Google',
+    },
+  },
+}
